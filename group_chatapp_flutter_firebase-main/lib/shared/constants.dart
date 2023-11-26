57b90a6548c7f1c0578bdfd7f1e0a655a0471937
Janaara <zanarzaparova4@gmail.com>
2023-11-26 20:05:37 +0600

    Initial commit

---
+++ group_chatapp_flutter_firebase-main/lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:905471288182:web:a021ec69318da5c5e0d057";
+  static String apiKey = "AIzaSyCI5TniW6Fk3bPLdg00PjXcXN5NMFtxUm0";
+  static String messagingSenderId = "905471288182";
+  static String projectId = "aiuconnect-3055d";
+  final primaryColor = const Color(0xFFee7b64);
+}
